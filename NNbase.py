fixed softmax bug

NNbase.py
@@ -14,13 +14,21 @@ def accuracy(ybatch, y):
     maxvals = np.repeat(np.max(y, axis=1).reshape((nbatch,1)), k, axis=1)
     return np.mean(np.sum(ybatch * (y==maxvals), axis=1))
 
+    # note: for the softmax probability conservation comes from summing along the
+    #second axis only -- assumes a 'default' input shape of (nsamp, batch)
+def softmax(x):
+    nsamp, k = x.shape
+    p=np.exp(x)
+    return p/ np.repeat(np.sum(p, axis=1).reshape(nsamp, 1), k, axis=1)
 
 class FCNetwork:
     """fully connected network"""
 
+    #implementations of relevant nonlinearities
+
     nl_functions = {'relu': lambda x: np.maximum(0, x),
                     'sigmoid': lambda x : 1.0 / (1.0 + np.exp(-x)),
-                    'softmax': lambda x: np.exp(x)/ np.sum(np.exp(x))}
+                    'softmax': softmax}
     nl_gradients = {'relu': lambda x, y: np.array(x>0, dtype=np.float32),
                     'sigmoid': lambda x, y: y * (1 - y)}
 
@@ -155,6 +163,10 @@ class FCNetwork:
             self.y[i]=y
             inputs = y
 
+    def get_output(self):
+        """ Returns the outputs of the final layer of neurons"""
+        return self.y[self.num_layers]
+
     def backward_pass(self, ybatch):
         """ Runs a single backward pass of the network, using the training y-values of ybatch.
             """
@@ -194,7 +206,7 @@ class FCNetwork:
             Assumes a KL ( or crossentropy) loss function"""
         if self.cost_function != 'dkl':
             raise ValueError("cost function {0} not implemented for softmax".format(self.cost_function))
-        yself = self.y[self.num_layers]
+        yself = self.get_output()
         if y.shape != yself.shape:
             raise ValueError
         return (yself - y)
@@ -208,7 +220,7 @@ class FCNetwork:
 
     def get_cost_function(self, ybatch):
         f = self.nonlinearities[self.num_layers]
-        y = self.y[self.num_layers]
+        y = self.get_output()
         batch_cost_functions = {'softmax': lambda ybatch, y: dkl(ybatch, y)}
         return batch_cost_functions[f](ybatch, y)
 
@@ -234,7 +246,7 @@ class FCNetwork:
         return self.get_cost_function(ybatch)
 
     def compute_accuracy(self, ybatch):
-        return accuracy(ybatch, self.y[self.num_layers])
+        return accuracy(ybatch, self.get_output())
 
     def update_weights_SGD(self, lr):
         """ performs a gradient-descent update of the weights with learning rate lr"""

classification_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from NNbase import FCNetwork, BatchGen
 
 import matplotlib.pyplot as plt
+plt.ion()
 
 max_samples=1000
 
@@ -45,21 +46,26 @@ images_test= maxnormalize(images_test)
 images_tr = maxnormalize(images_tr)
 
 
-Nsamp=3
+Nsamp=100
 x,y = images_tr[:Nsamp, :, :], labels_tr[:Nsamp ]
 x = np.reshape(x, (Nsamp, 28 * 28))
 y = digit_onehot_embedding(y)
 
-num_layers = 3
-layer_sizes = [784, 256, 256, 10]
-activations = ['relu', 'relu', 'softmax']
+num_layers = 2
+layer_sizes = [784, 256, 10]
+activations = [ 'relu', 'softmax']
 fcnet = FCNetwork(num_layers)
 fcnet.set_layer_sizes(layer_sizes)
 fcnet.set_cost_function('dkl')
 fcnet.set_activation_functions(activations)
 fcnet.initialize_all()
-batch_size =3
-epochs=100
+batch_size =32
+epochs=1000
 batchgen = BatchGen(x, y, batch_size).get_batches
+x,y=batchgen()
+xb,yb = x[0], y[0]
 
-costs=fcnet.train_SGD(batchgen, epochs, lr=1e-2)
+# plt.hist(fcnet.weights[2].flatten())
+costs, acc=fcnet.train_SGD(batchgen, epochs, lr=1e-2)
+plt.plot(costs)
+plt.plot(acc)